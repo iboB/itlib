7a01c334568c8a96ffaad68ac2f299c4b51b17a0
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-10-21 15:59:34 +0300

    Added rstream Closes #6

---
+++ test/t-rstream-11.cpp
@@ -0,0 +1,56 @@
+#include "doctest.hpp"
+
+#include <itlib/rstream.hpp>
+
+#include <sstream>
+
+TEST_SUITE_BEGIN("rstream");
+
+const char* text = "1234567890abcdefghij";
+
+TEST_CASE("no rebase")
+{
+    std::istringstream sin(text);
+
+    char data[6] = "-----";
+    const char* str = data;
+    sin.read(data, 5);
+    CHECK(str == "12345");
+
+    {
+        itlib::rstream rin(sin);
+        rin.read(data, 5);
+        CHECK(str == "67890");
+    }
+
+    sin.read(data, 5);
+    CHECK(str == "abcde");
+
+    //{
+    //    itlib::rstream rin(sin);
+    //    char c;
+    //    rin >> c;
+    //    CHECK(c == 'f');
+    //}
+}
+
+TEST_CASE("yes rebase")
+{
+    std::istringstream sin(text);
+
+    char data[6] = "-----";
+    const char* str = data;
+
+    {
+        itlib::redirect_rstream rin(sin, 5);
+        rin.read(data, 5);
+        CHECK(str == "67890");
+
+        //char c;
+        //rin >> c;
+        //CHECK(c == 'a');
+    }
+
+    sin.read(data, 5);
+    CHECK(str == "12345");
+}
