3f72ff97999ffcc7666d772c53af66b48bf6d3f6
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-12-28 11:15:27 +0200

    Added type traits

---
+++ test/t-type_traits-11.cpp
@@ -0,0 +1,20 @@
+#include "doctest.hpp"
+
+#include <itlib/type_traits.hpp>
+
+#include <vector>
+#include <string>
+
+TEST_CASE("is_instantiation_of")
+{
+    CHECK_FALSE(itlib::is_instantiation_of<std::vector, int>::value);
+
+    using ivec = std::vector<int>;
+    using vecvec = std::vector<ivec>;
+    CHECK(itlib::is_instantiation_of<std::vector, ivec>::value);
+    CHECK(itlib::is_instantiation_of<std::vector, vecvec>::value);
+    CHECK_FALSE(itlib::is_instantiation_of<std::vector, std::string>::value);
+
+    CHECK(itlib::is_instantiation_of<std::basic_string, std::string>::value);
+    CHECK_FALSE(itlib::is_instantiation_of<std::basic_string, ivec>::value);
+}
---
+++ test/t-type_traits-17.cpp
@@ -0,0 +1,20 @@
+#include "doctest.hpp"
+
+#include <itlib/type_traits.hpp>
+
+#include <vector>
+#include <string>
+
+TEST_CASE("is_instantiation_of")
+{
+    CHECK_FALSE(itlib::is_instantiation_of_v<std::vector, int>);
+
+    using ivec = std::vector<int>;
+    using vecvec = std::vector<ivec>;
+    CHECK(itlib::is_instantiation_of_v<std::vector, ivec>);
+    CHECK(itlib::is_instantiation_of_v<std::vector, vecvec>);
+    CHECK_FALSE(itlib::is_instantiation_of_v<std::vector, std::string>);
+
+    CHECK(itlib::is_instantiation_of_v<std::basic_string, std::string>);
+    CHECK_FALSE(itlib::is_instantiation_of_v<std::basic_string, ivec>);
+}
