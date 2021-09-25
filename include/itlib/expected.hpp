da9017d27b0b45efb3d1ddf138bbddb297ee9f2c
Borislav Stanimirov <b.stanimirov@abv.bg>
2021-09-25 15:59:15 +0300

    WiP for expected

---
+++ include/itlib/expected.hpp
@@ -0,0 +1,209 @@
+// itlib-expected v1.00
+//
+// A union-type of a value and an error
+//
+// MIT License:
+// Copyright(c) 2021 Borislav Stanimirov
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and / or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions :
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+//                  VERSION HISTORY
+//
+//  1.00 (2021-09-25) Initial-release
+//
+//
+//                  DOCUMENTATION
+//
+// Simply include this file wherever you need.
+// It defines the class itlib::expected,
+//
+//
+//                  TESTS
+//
+// You can find unit tests for expected in its official repo:
+// https://github.com/iboB/itlib/blob/master/test/
+//
+#pragma once
+
+#include <cassert>
+#include <utility>
+#include <new>
+
+namespace itlib
+{
+
+template <typename E>
+class unexpected_t
+{
+public:
+    explicit unexpected_t(E&& e) : m_error(std::forward<E>(e)) {}
+
+private:
+    template<typename T, typename E2>
+    friend class expected;
+
+    E m_error;
+};
+
+template <typename E>
+unexpected_t<E> unexpected(E&& e)
+{
+    return unexpected_t<E>(std::forward<E>(e));
+}
+
+struct unexpect {};
+unexpect unexpected() { return {}; }
+
+template <typename T, typename E>
+class expected
+{
+public:
+    using value_type = T;
+    using error_type = E;
+
+    expected() : m_value(), m_has_value(true) {}
+    expected(T&& t) : m_value(std::forward<T>(t)), m_has_value(true) {}
+
+    template <typename E2>
+    expected(unexpected_t<E2>&& u) : m_error(std::move(u.m_error)), m_has_value(false) {}
+
+    expected(unexpect) : m_error(), m_has_value(false) {}
+
+    // do not copy
+    expected(const expected&) = delete;
+    expected& operator=(const expected&) = delete;
+
+    // do move
+    expected(expected&& other) noexcept
+        : m_has_value(other.has_value())
+    {
+        if (m_has_value)
+        {
+            new (&m_value) T(std::move(other.m_value));
+        }
+        else
+        {
+            new (&m_error) E(std::move(other.m_error));
+        }
+    }
+
+    expected& operator=(expected&& other) noexcept
+    {
+        if (m_has_value && other.has_value())
+        {
+            m_value = std::move(other.m_value);
+        }
+        else if(m_has_value && !other.has_value())
+        {
+            m_has_value = false;
+            m_value.~T();
+            ::new (&m_error) E(std::move(other.m_error));
+        }
+        else if(!m_has_value && other.has_value())
+        {
+            m_has_value = true;
+            m_error.~E();
+            ::new (&m_value) T(std::move(other.m_value));
+        }
+        else
+        {
+            m_error = std::move(other.m_error);
+        }
+        return *this;
+    }
+
+    ~expected()
+    {
+        if (m_has_value)
+        {
+            m_value.~T();
+        }
+        else
+        {
+            m_error.~E();
+        }
+    }
+
+    // bool interface
+    bool has_value() const { return m_has_value; }
+    bool has_error() const { return !m_has_value; }
+    explicit operator bool() const { return m_has_value; }
+
+    // value getters
+    T& value() &
+    {
+        assert(has_value());
+        return m_value;
+    }
+
+    const T& value() const &
+    {
+        assert(has_value());
+        return m_value;
+    }
+
+    T&& value() &&
+    {
+        assert(has_value());
+        return std::move(m_value);
+    }
+
+    T& operator*() & { return value(); }
+    const T& operator*() const & { return value(); }
+    T&& operator*() && { return std::move(value()); }
+
+    T& value_or(T& v) & { return has_value() ? value() : v; }
+    const T& value_or(const T& v) const & { return has_value() ? value() : v; }
+    T&& value_or(T&& v) && { return has_value() ? value() : std::move(v); }
+
+    T* operator->() { return &value(); }
+    const T* operator->() const { return &value(); }
+
+    // error getters
+
+    E& error() &
+    {
+        assert(has_error());
+        return m_error;
+    }
+
+    const E& error() const &
+    {
+        assert(has_error());
+        return m_error;
+    }
+
+    E&& error() &&
+    {
+        assert(has_error());
+        return std::move(m_error);
+    }
+
+private:
+    union
+    {
+        value_type m_value;
+        error_type m_error;
+    };
+    bool m_has_value;
+};
+
+}
---
+++ test/t-expected-11.cpp
@@ -0,0 +1,176 @@
+#include "doctest.hpp"
+
+#include <itlib/expected.hpp>
+
+#include <type_traits>
+#include <string>
+
+using namespace itlib;
+
+enum class ecode
+{
+    error_a,
+    error_b,
+    error_c,
+};
+
+TEST_CASE("i/e")
+{
+    using iee = expected<int, ecode>;
+    static_assert(std::is_same<iee::value_type, int>::value);
+    static_assert(std::is_same<iee::error_type, ecode>::value);
+
+    iee a;
+    REQUIRE(!!a);
+    CHECK(a.has_value());
+    CHECK(!a.has_error());
+    CHECK(a.value() == 0);
+    CHECK(*a == 0);
+    CHECK(a.value_or(74) == 0);
+
+    iee b = 123;
+    REQUIRE(!!b);
+    CHECK(b.has_value());
+    CHECK(!b.has_error());
+    CHECK(b.value() == 123);
+    CHECK(*b == 123);
+    CHECK(b.value_or(74) == 123);
+
+    iee e = unexpected(ecode::error_b);
+    REQUIRE(!e);
+    CHECK(!e.has_value());
+    CHECK(e.has_error());
+    CHECK(e.error() == ecode::error_b);
+    CHECK(e.value_or(74) == 74);
+}
+
+TEST_CASE("i/d")
+{
+    using ide = expected<int, double>;
+    static_assert(std::is_same<ide::value_type, int>::value);
+    static_assert(std::is_same<ide::error_type, double>::value);
+
+    ide a = 2.3;
+    CHECK(a.value_or(5) == 2);
+
+    ide b = unexpected(7);
+    REQUIRE(b.has_error());
+}
+
+TEST_CASE("string")
+{
+    using ise = expected<int, std::string>;
+    static_assert(std::is_same<ise::value_type, int>::value);
+    static_assert(std::is_same<ise::error_type, std::string>::value);
+
+    ise a = 432;
+    CHECK(!!a);
+
+    ise b = unexpected("foo");
+    REQUIRE(!b);
+    CHECK(b.error() == "foo");
+
+    ise u = unexpect{};
+    REQUIRE(!u);
+    CHECK(u.error().empty());
+
+    using sie = expected<std::string, int>;
+    static_assert(std::is_same<sie::value_type, std::string>::value);
+    static_assert(std::is_same<sie::error_type, int>::value);
+
+    sie c;
+    REQUIRE(c.has_value());
+    CHECK(c->empty());
+
+    sie d = unexpected(23);
+    REQUIRE(d.has_error());
+
+    sie u2 = unexpected();
+    REQUIRE(!u2);
+    CHECK(u2.error() == 0);
+}
+
+
+// tracks lifetime
+template <typename C>
+struct track_base
+{
+    track_base() { ++def_constructions; }
+    track_base(const track_base&) { ++copy_constructions; }
+    track_base(track_base&&) { ++move_constructions; }
+    track_base& operator=(const track_base&) { ++copy_assignments; return *this; }
+    track_base& operator=(track_base&&) { ++move_assignments; return *this; }
+    ~track_base() { ++destructions; }
+    static void reset_counters()
+    {
+        def_constructions = 0;
+        move_constructions = 0;
+        move_assignments = 0;
+        destructions = 0;
+
+        // explicitly not cleared
+        // should always be zero
+        // copy_constructions = 0;
+        // copy_assignments = 0;
+    }
+
+    static int def_constructions;
+    static int copy_constructions;
+    static int move_constructions;
+    static int copy_assignments;
+    static int move_assignments;
+    static int destructions;
+};
+template <typename C> int track_base<C>::def_constructions;
+template <typename C> int track_base<C>::copy_constructions;
+template <typename C> int track_base<C>::move_constructions;
+template <typename C> int track_base<C>::copy_assignments;
+template <typename C> int track_base<C>::move_assignments;
+template <typename C> int track_base<C>::destructions;
+
+struct value : track_base<value> {};
+struct error : track_base<error> {};
+
+using vee = expected<value, error>;
+static_assert(std::is_same<vee::value_type, value>::value);
+static_assert(std::is_same<vee::error_type, error>::value);
+
+vee func(bool b)
+{
+    if(b) return {};
+    else return unexpected();
+}
+
+TEST_CASE("lifetime")
+{
+    {
+        const auto x = func(false);
+        CHECK(!x);
+    }
+    CHECK(error::def_constructions == 1);
+    CHECK(error::move_constructions == 0);
+    CHECK(error::destructions == 1);
+    CHECK(value::def_constructions == 0);
+    CHECK(value::move_constructions == 0);
+    CHECK(value::destructions == 0);
+
+    {
+        const auto x = func(true);
+        CHECK(!!x);
+    }
+
+    CHECK(error::def_constructions == 1);
+    CHECK(error::move_constructions == 0);
+    CHECK(error::destructions == 1);
+    CHECK(value::def_constructions == 1);
+    CHECK(value::move_constructions == 0);
+    CHECK(value::destructions == 1);
+
+    value::reset_counters();
+    error::reset_counters();
+
+    CHECK(value::copy_constructions == 0);
+    CHECK(value::copy_assignments == 0);
+    CHECK(error::copy_constructions == 0);
+    CHECK(error::copy_assignments == 0);
+}
