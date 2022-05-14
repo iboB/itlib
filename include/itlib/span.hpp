ea30aacb70fc0fb5e30209e87023b880cc277e8c
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-05-14 14:14:23 +0300

    New library: span Ref #12

---
+++ include/itlib/span.hpp
@@ -0,0 +1,300 @@
+// itlib-span v1.00
+//
+// A C++11 implementation C++20's of std::span with a dynamic extent
+//
+// SPDX-License-Identifier: MIT
+// MIT License:
+// Copyright(c) 2022 Borislav Stanimirov
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and / or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions :
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+//                  VERSION HISTORY
+//
+//  1.00 (2022-05-14) Initial release
+//
+//
+//                  DOCUMENTATION
+//
+// Simply include this file wherever you need.
+//
+// This class is designed as a drop-in replacement of std::span from C++20
+// witha a dynamic extent. For a reference of std::span see here:
+// https://en.cppreference.com/w/cpp/container/span
+//
+// itlib does not (yet) provide a static-extent span
+//
+//                  Configuration
+//
+// itlib::span has a single configurable setting:
+//
+//                  Config bounds checks:
+//
+// By default bounds checks are made in debug mode (via an assert) when
+// accessing elements (with `[]`). Iterators are not checked (yet...)
+//
+// To disable them, you can define ITLIB_SPAN_NO_DEBUG_BOUNDS_CHECK before
+// including the header.
+//
+//
+//                  TESTS
+//
+// You can find unit tests for span in its official repo:
+// https://github.com/iboB/itlib/blob/master/test/
+//
+#pragma once
+
+#include <cstddef>
+#include <iterator>
+#include <type_traits>
+
+#if defined(ITLIB_SPAN_NO_DEBUG_BOUNDS_CHECK)
+#   define I_ITLIB_SPAN_BOUNDS_CHECK(i)
+#else
+#   include <cassert>
+#   define I_ITLIB_SPAN_BOUNDS_CHECK(i) assert((i) < this->size())
+#endif
+
+namespace itlib
+{
+
+template <typename T>
+class span
+{
+public:
+    using element_type = T;
+    using value_type = typename std::remove_cv<T>::type;
+    using size_type = size_t;
+    using difference_type = ptrdiff_t;
+    using reference = T&;
+    using const_reference = const T&;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using iterator = T*;
+    using const_iterator = const T*;
+    using reverse_iterator = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    span() noexcept = default;
+
+    template <typename InputIterator, typename = decltype(*std::declval<InputIterator>())>
+    span(InputIterator begin, InputIterator end)
+        : m_begin(&(*begin))
+        , m_end(m_begin + (end - begin))
+    {}
+
+    template <typename InputIterator, typename = decltype(*std::declval<InputIterator>())>
+    span(InputIterator begin, size_t size)
+        : span(begin, begin + size)
+    {}
+
+    template <typename Container, typename = decltype(std::declval<Container>().data())>
+    span(Container& c)
+        : span(c.begin(), c.end())
+    {}
+
+    template <size_t N>
+    span(T(&ar)[N])
+        : span(ar, N)
+    {}
+
+    span(const span&) noexcept = default;
+    span& operator=(const span&) noexcept = default;
+
+    template <typename U>
+    typename std::enable_if<std::is_same<typename std::remove_cv<T>::type, U>::value,
+        span&>::type operator=(const span<U>& other) noexcept
+    {
+        m_begin = other.begin();
+        m_end = other.end();
+        return *this;
+    }
+
+    span(span&&) noexcept = default;
+    span& operator=(span&&) noexcept = default;
+
+    explicit operator bool() const
+    {
+        return !!m_begin;
+    }
+
+    const T& at(size_t i) const
+    {
+        I_ITLIB_SPAN_BOUNDS_CHECK(i);
+        return *(m_begin + i);
+    }
+
+    T& at(size_t i)
+    {
+        I_ITLIB_SPAN_BOUNDS_CHECK(i);
+        return *(m_begin + i);
+    }
+
+    const T& operator[](size_t i) const
+    {
+        return at(i);
+    }
+
+    T& operator[](size_t i)
+    {
+        return at(i);
+    }
+
+    const T& front() const
+    {
+        return at(0);
+    }
+
+    T& front()
+    {
+        return at(0);
+    }
+
+    const T& back() const
+    {
+        return at(size() - 1);
+    }
+
+    T& back()
+    {
+        return at(size() - 1);
+    }
+
+    const T* data() const noexcept
+    {
+        return m_begin;
+    }
+
+    T* data() noexcept
+    {
+        return m_begin;
+    }
+
+    // iterators
+    iterator begin() noexcept
+    {
+        return m_begin;
+    }
+
+    const_iterator begin() const noexcept
+    {
+        return m_begin;
+    }
+
+    iterator end() noexcept
+    {
+        return m_end;
+    }
+
+    const_iterator end() const noexcept
+    {
+        return m_end;
+    }
+
+    reverse_iterator rbegin() noexcept
+    {
+        return reverse_iterator(end());
+    }
+
+    const_reverse_iterator rbegin() const noexcept
+    {
+        return const_reverse_iterator(end());
+    }
+
+    reverse_iterator rend() noexcept
+    {
+        return reverse_iterator(begin());
+    }
+
+    const_reverse_iterator rend() const noexcept
+    {
+        return const_reverse_iterator(begin());
+    }
+
+    // capacity
+    bool empty() const noexcept
+    {
+        return m_begin == m_end;
+    }
+
+    size_t size() const noexcept
+    {
+        return m_end - m_begin;
+    }
+
+    // slicing
+    span subspan(size_t off, size_t count = size_t(-1)) const noexcept
+    {
+        if (m_begin + off > m_end) return span(m_end, 0);
+        auto newSize = size() - off;
+        if (count > newSize) count = newSize;
+        return span(m_begin + off, count);
+    }
+
+    span first(size_t n) const noexcept
+    {
+        return subspan(0, n);
+    }
+
+    span last(size_t n) const noexcept
+    {
+        return subspan(size() - n, n);
+    }
+
+    void remove_prefix(size_t n) noexcept
+    {
+        m_begin += n;
+    }
+
+    void remove_suffix(size_t n) noexcept
+    {
+        m_end -= n;
+    }
+
+private:
+    T* m_begin = nullptr;
+    T* m_end = nullptr;
+};
+
+template <typename T>
+span<T> make_span(T* begin, T* end)
+{
+    return span<T>(begin, end);
+}
+
+template <typename T>
+span<T> make_span(T* begin, size_t size)
+{
+    return span<T>(begin, size);
+}
+
+template <typename Container, typename Ptr = decltype(std::declval<Container>().data())>
+auto make_span(Container& c) -> span<typename std::remove_pointer<Ptr>::type>
+{
+    return span<typename std::remove_pointer<Ptr>::type>(c);
+}
+
+template <typename T, size_t N>
+span<T> make_span(T(&ar)[N])
+{
+    return span<T>(ar);
+}
+
+}
---
+++ test/t-span-11.cpp
@@ -0,0 +1,192 @@
+#include "doctest.hpp"
+
+#include <itlib/span.hpp>
+
+#include <vector>
+
+TEST_CASE("[span] construction")
+{
+    using namespace itlib;
+
+    {
+        span<int> e;
+        CHECK(!e);
+        CHECK(e.size() == 0);
+        CHECK(e.begin() == e.end());
+        CHECK(e.rbegin() == e.rend());
+        CHECK(e.empty());
+        CHECK(e.data() == nullptr);
+    }
+
+    int i[] = {0,2,3,4};
+    {
+        span<int> ints(i);
+        CHECK(ints);
+        CHECK(ints.size() == 4);
+        CHECK(ints.begin() + 4 == ints.end());
+        CHECK(ints.rbegin() + 4 == ints.rend());
+        CHECK(!ints.empty());
+        CHECK(ints.data() == i);
+
+        ints.front() = 1;
+        CHECK(i[0] == 1);
+    }
+
+    {
+        span<const int> ints(i);
+        CHECK(ints);
+        CHECK(ints.size() == 4);
+        CHECK(ints.begin() + 4 == ints.end());
+        CHECK(ints.rbegin() + 4 == ints.rend());
+        CHECK(!ints.empty());
+        CHECK(ints.data() == i);
+    }
+
+    std::vector<int> vec = {1, 2, 3};
+    {
+        span<int> ints(vec);
+        CHECK(ints);
+        CHECK(ints.size() == 3);
+        CHECK(ints.begin() + 3 == ints.end());
+        CHECK(ints.rbegin() + 3 == ints.rend());
+        CHECK(!ints.empty());
+        CHECK(ints.data() == vec.data());
+
+        ints.back() = 17;
+        CHECK(vec[2] == 17);
+    }
+}
+
+TEST_CASE("[span] make_span")
+{
+    using namespace itlib;
+
+    std::vector<int> vec = {1, 2, 3};
+
+    {
+        auto s = make_span(vec);
+        CHECK(s.size() == 3);
+        static_assert(std::is_same<span<int>, decltype(s)>::value, "make_span(vec<int>)");
+    }
+
+    {
+        auto s = make_span(vec.data(), vec.size());
+        CHECK(s.size() == 3);
+        static_assert(std::is_same<span<int>, decltype(s)>::value, "make_span(int*, s)");
+    }
+
+    {
+        auto s = make_span(vec.data(), vec.data() + vec.size());
+        CHECK(s.size() == 3);
+        static_assert(std::is_same<span<int>, decltype(s)>::value, "make_span(int*, int*)");
+    }
+
+    const std::vector<int> cvec = {5, 6, 7, 8};
+
+    {
+        auto s = make_span(cvec);
+        CHECK(s.size() == 4);
+        static_assert(std::is_same<span<const int>, decltype(s)>::value, "make_span(const vec<int>)");
+    }
+
+    {
+        auto s = make_span(cvec.data(), cvec.size());
+        CHECK(s.size() == 4);
+        static_assert(std::is_same<span<const int>, decltype(s)>::value, "make_span(const int*, s)");
+    }
+
+    {
+        auto s = make_span(cvec.data(), cvec.data() + cvec.size());
+        CHECK(s.size() == 4);
+        static_assert(std::is_same<span<const int>, decltype(s)>::value, "make_span(const int*, const int*)");
+    }
+}
+
+size_t ifunc(itlib::span<const int> t) { return t.size(); }
+
+TEST_CASE("[span] funcs")
+{
+    using namespace itlib;
+
+    std::vector<int> vec = {1, 2, 3};
+    CHECK(ifunc(vec) == 3);
+
+    auto s = make_span(vec);
+    CHECK(ifunc(s) == 3);
+}
+
+TEST_CASE("[span] copy-ctor and assign")
+{
+    using namespace itlib;
+
+    std::vector<int> vec = {1, 2, 3};
+    auto s = make_span(vec);
+
+    auto s2 = s;
+    CHECK(s2.size() == 3);
+    CHECK(s2.data() == s.data());
+
+    span<int> sc;
+    sc = s2;
+    CHECK(sc.size() == 3);
+    CHECK(sc.data() == s.data());
+
+    span<const int> cs;
+    cs = s2;
+    CHECK(cs.size() == 3);
+    CHECK(cs.data() == s.data());
+
+    span<const int> cs2 = vec;
+    CHECK(cs2.size() == 3);
+    CHECK(cs2.data() == s.data());
+}
+
+template <typename S>
+void test_slicing(const S& span)
+{
+    {
+        auto s = span.subspan(10);
+        CHECK(!!s);
+        CHECK(s.empty());
+        CHECK(s.begin() == span.end());
+    }
+    {
+        auto s = span.subspan(1);
+        CHECK(s.size() == 4);
+        CHECK(s.begin() == span.begin() + 1);
+    }
+    {
+        auto s = span.subspan(3, 1);
+        CHECK(s.size() == 1);
+        CHECK(s.begin() == span.begin() + 3);
+    }
+    {
+        auto s = span.first(3);
+        CHECK(s.size() == 3);
+        CHECK(s.begin() == span.begin());
+    }
+    {
+        auto s = span.last(2);
+        CHECK(s.size() == 2);
+        CHECK(s.begin() == span.begin() + 3);
+    }
+    {
+        auto cp = span;
+        cp.remove_prefix(2);
+        CHECK(cp.size() == 3);
+        CHECK(cp.begin() == span.begin() + 2);
+        cp.remove_suffix(2);
+        CHECK(cp.size() == 1);
+        CHECK(cp.end() == span.begin() + 3);
+    }
+}
+
+TEST_CASE("[span] slicing")
+{
+    using namespace itlib;
+    std::vector<int> ivec = {6, 7, 8, 9, 10};
+    auto s = make_span(ivec);
+    test_slicing(s);
+    span<const int> cs = s;
+    test_slicing(cs);
+}
