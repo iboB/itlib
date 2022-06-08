8e5024851bf24e4fa0378b37038be20cadb0f8b8
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-06-08 15:11:09 +0300

    [pod_vector] realloc fallback optimized

--- include/itlib/pod_vector.hpp
+++ include/itlib/pod_vector.hpp
@@ -841,10 +841,11 @@ private:
             // we could memmove if this is the case, but for now we will just malloc-copy
             // it should be rare anyway
             auto new_buf = a_malloc(num_elements);
-            auto s = size();
-            auto mins = s < num_elements ? s : num_elements;
-            std::memcpy(new_buf, m_begin, e2b(mins));
-            a_free_begin();
+            if (m_begin)
+            {
+                std::memcpy(new_buf, m_begin, e2b(size()));
+                a_free_begin();
+            }
             m_begin = pointer(new_buf);
         }
 
