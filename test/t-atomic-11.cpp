bb3d32d61884ad678d0581aaa1511b4d01f0597e
Borislav Stanimirov <b.stanimirov@abv.bg>
2022-12-06 21:12:03 +0200

    [atomic] test: fixed gcc/clang build

--- test/t-atomic-11.cpp
+++ test/t-atomic-11.cpp
@@ -5,10 +5,10 @@
 #include <thread>
 
 TEST_CASE("atomic_relaxed_counter") {
-    itlib::atomic_relaxed_counter<int> cnta = 0;
-    itlib::atomic_relaxed_counter<uint8_t> cntb = 0; // will likely overflow
+    itlib::atomic_relaxed_counter<int> cnta{0};
+    itlib::atomic_relaxed_counter<uint8_t> cntb{0}; // will likely overflow
 
-    itlib::atomic_relaxed_counter<bool> start = false;
+    itlib::atomic_relaxed_counter<bool> start{false};
 
     std::thread a([&]() {
         while (!start);
