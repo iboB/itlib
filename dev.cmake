8708a4626826b79892af1a85072822f66a57a0e7
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-10-24 11:09:22 +0300

    update dev.cmake as per icm_dev_mode

--- dev.cmake
+++ dev.cmake
@@ -1,17 +1,16 @@
+# standard
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 
-option(ITLIB_TSAN "itlib: build with thread sanitizer on" OFF)
-option(ITLIB_ASAN "itlib: build with address sanitizer on" OFF)
-option(ITLIB_CLANG_TIDY "itlib: use clang tidy" OFF)
+# misc config
+set_property(GLOBAL PROPERTY USE_FOLDERS ON) # use solution folders
 
-set(ITLIB_SAN_FLAGS "")
-if(MSVC)    
+if(MSVC)
     add_compile_options(
-        /W4
-        -D_CRT_SECURE_NO_WARNINGS /Zc:__cplusplus /permissive-
-        /volatile:iso /Zc:throwingNew /utf-8 -DNOMINMAX=1
-        /wd4251 /wd4275
+        -W4
+        -D_CRT_SECURE_NO_WARNINGS -Zc:__cplusplus -permissive-
+        -volatile:iso -Zc:throwingNew -Zc:templateScope -utf-8 -DNOMINMAX=1
+        -wd4251 -wd4275
     )
 else()
     add_compile_options(-Wall -Wextra -Wno-type-limits)
@@ -20,22 +19,44 @@ else()
     endif()
 endif()
 
-if(ITLIB_TSAN)
-    if(NOT MSVC)
-        set(ITLIB_SAN_FLAGS -fsanitize=thread -g)
-    endif()
-elseif(ITLIB_ASAN)
-    if(MSVC)
-        set(ITLIB_SAN_FLAGS /fsanitize=address)
-    elseif(APPLE)
-        # apple clang doesn't support the leak sanitizer
-        set(ITLIB_SAN_FLAGS -fsanitize=address,undefined -pthread -g)
-    else()
-        set(ITLIB_SAN_FLAGS -fsanitize=address,undefined,leak -pthread -g)
-    endif()
-endif()
 
-add_compile_options(${ITLIB_SAN_FLAGS})
-if(NOT MSVC)
-    add_link_options(${ITLIB_SAN_FLAGS})
+# sanitizers
+
+option(SAN_THREAD "itlib: sanitize thread" OFF)
+option(SAN_ADDR "itlib: sanitize address" OFF)
+option(SAN_UB "itlib: sanitize undefined behavior" OFF)
+option(SAN_LEAK "itlib: sanitize leaks" OFF)
+
+if(MSVC)
+    if(SAN_ADDR)
+        add_compile_options(-fsanitize=address)
+    endif()
+    if(SAN_THREAD OR SAN_UB OR SAN_LEAK)
+        message(WARNING "Unsupported sanitizers requested for msvc. Ignored")
+    endif()
+else()
+    if(SAN_THREAD)
+        set(itlibSanFlags -fsanitize=thread -g)
+        if(SAN_ADDR OR SAN_UB OR SAN_LEAK)
+            message(WARNING "Incompatible sanitizer combination requested. Only 'SAN_THREAD' will be respected")
+        endif()
+    else()
+        if(SAN_ADDR)
+            list(APPEND itlibSanFlags -fsanitize=address -pthread)
+        endif()
+        if(SAN_UB)
+            list(APPEND itlibSanFlags -fsanitize=undefined)
+        endif()
+        if(SAN_LEAK)
+            if(APPLE)
+                message(WARNING "Unsupported leak sanitizer requested for Apple. Ignored")
+            else()
+                list(APPEND itlibSanFlags -fsanitize=leak)
+            endif()
+        endif()
+    endif()
+    if(itlibSanFlags)
+        add_compile_options(${itlibSanFlags})
+        add_link_options(${itlibSanFlags})
+    endif()
 endif()
