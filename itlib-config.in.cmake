b2e54cec7cfeff00765339d243ec958e823fda33
Borislav Stanimirov <b@ibob.bg>
2025-06-12 10:34:16 +0300

    cmake: bump min version, list public headers, add install

---
+++ itlib-config.in.cmake
@@ -0,0 +1,5 @@
+# Copyright (c) Borislav Stanimirov
+# SPDX-License-Identifier: MIT
+#
+include("${CMAKE_CURRENT_LIST_DIR}/itlib-targets.cmake")
+message(STATUS "alpaca-core-sdk @PROJECT_VERSION@ found in ${CMAKE_CURRENT_LIST_DIR}")
