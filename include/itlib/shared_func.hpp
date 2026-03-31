815e6543e6a906fae7a8686ce95eba6049c6b872
Borislav Stanimirov <b@ibob.bg>
2026-03-31 16:25:55 +0300

    new library: shared_func, wip

---
+++ include/itlib/shared_func.hpp
@@ -0,0 +1,97 @@
+// itlib-shared_func v1.00
+//
+// A std::functon alternative which keeps the function object data in a
+// shared_ptr, combining the functionality of both.
+//
+// SPDX-License-Identifier: MIT
+// MIT License:
+// Copyright(c) 2026 Borislav Stanimirov
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files(the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and / or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions :
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT.IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//
+//                  VERSION HISTORY
+//
+//  1.00 (2026-03-31) Initial release
+//
+//
+//                  DOCUMENTATION
+//
+// Simply include this file wherever you need.
+// It defines the class itlib::shared_func. A shared_func is an alternative to
+// std::function which keeps the function object data in a shared_ptr.
+//
+// This means that the same function object can be shared between multiple
+// shared_func instances, allowing copy semantics without copying the function
+// object data. This can be useful when there are huge captures or non-copyable
+// captures.
+//
+// Still, the function is shared so the captures are shared too. This may
+// introduce race conditions if the function is called from multiple threads,
+// so use the mental model for shared_ptr when you're using shared_func.
+//
+//                  TESTS
+//
+// You can find unit tests in the official repo:
+// https://github.com/iboB/itlib/blob/master/test/
+//
+#pragma once
+#include <memory>
+
+namespace itlib {
+
+template <typename F>
+class shared_func;
+
+template <typename R, typename... Args>
+class shared_func<R(Args...)> {
+public:
+
+private:
+    struct wrapper_base {
+        // Note that we're not using virtual here, even though we have dynamic
+        // dispatch. The problem with virtual is that it will create a vtable
+        // pointer. It this is used across shared libraries (.so/.dll/.dylib),
+        // it guarantees that the typeid and vtable pointer will be different
+        // across the modules.
+        // This is not a problem in practice since the compiler will generate
+        // the same layout for the vtables, but if used with sanitizers or
+        // other type of runtime instrumentation, it can cause false positives
+        // for type mismatches.
+        // So... poor man's vtable:
+        R(*func)(void* obj, Args&&...);
+    protected:
+        // we can't afford a virtual dtor, so make sure that it's not available
+        ~wrapper_base() = default;
+    };
+    std::shared_ptr<wrapper_base> m_wrapper;
+
+    template <typename FO>
+    struct wrapper : public wrapper_base {
+        FO func_object;
+        wrapper(FO&& f) : func_object(std::forward<FO>(f)) {
+            this->func = +[](void* obj, Args&&... args) -> R {
+                return static_cast<wrapper*>(obj)->func_object(std::forward<Args>(args)...);
+            };
+        }
+    };
+};
+
+} // namespace itlib
